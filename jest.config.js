d3999ea41fc411f86039b09bda2d9a203335a523
Nikolay Lapshin <nlapshin1989@gmail.com>
2023-01-25 19:44:04 +0300

    Added allure

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,11 @@ export default {
   "transform": {
     "^.+\\.[t|j]sx?$": "babel-jest"
   },
+  "testRunner": "jest-jasmine2",
+  setupFilesAfterEnv: ["jest-allure/dist/setup"],
   reporters: [
     "default", 
+    "jest-allure",
     [
       "jest-html-reporters", {
         "publicPath": "./jest-html-report",
